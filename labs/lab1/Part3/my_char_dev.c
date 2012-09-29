0198a3e2150382b8d60c89937aecc0f7bcdabdee
strikkad <strikkad@andrew.cmu.edu>
2012-09-29 19:09:31 -0400

    committing added changes

--- labs/lab1/Part3/my_char_dev.c
+++ labs/lab1/Part3/my_char_dev.c
@@ -10,9 +10,13 @@
 #include <linux/kernel.h>       /* Needed for KERN_INFO */
 #include <linux/fs.h>
 #include <linux/sched.h>
-
-#define OK 		0
-#define NOK 		1
+#include <linux/types.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/slab_def.h>
+#include <linux/errno.h>
+#define OK 	0
+#define NOK 	1
 #define DEVICE_NAME 	"my_char_dev"	/* Dev name as it appears in /proc/devices */
 #define MAJOR_NUM 	123  		/* Hardcoded since we are the only once going to use this driver; a dynamic
 					assignment may require udev to create at runtime */
@@ -20,7 +24,7 @@
 
 static int device_open(struct inode *inode, struct file *file);
 static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t * offset);
-static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t *off);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 static int device_release(struct inode *inode, struct file *file);
 
 static struct file_operations fops = {
@@ -31,7 +35,11 @@ static struct file_operations fops = {
 };
 
 struct task_struct *my_task;
-
+char *read_buf = NULL;
+int flag = 0;
+int flag1=0;
+char *curr_ptr = NULL;
+int len = 0;
 int module_start(void)
 {
 	int ret = -1;
@@ -47,18 +55,64 @@ int module_start(void)
 static int device_open(struct inode *inode, struct file *file)
 {
 	printk(KERN_ALERT "File opened");	/* Do we have to do anything here */
+	read_buf = kmalloc((size_t)(8192),GFP_ATOMIC);
+	if(read_buf ==NULL)
+		return -ENOMEM;
+	flag = 0;
+	flag1 = 0;
+	len = 0;
 	return OK;
 }
 
 static int device_read(struct file *filp, char *buffer, size_t length, loff_t * offset)
 {	
-	for_each_process(my_task){
-        	printk(KERN_INFO "Hello World from Process [%d] with priority [%d]\r\n", my_task->pid, my_task->prio);
+	char *str = NULL; 
+	if(flag == 0)
+	{
+		str = kmalloc((size_t)(128),GFP_ATOMIC);
+		if(str ==NULL)
+			return -ENOMEM;
+		strcpy(read_buf,"");
+		for_each_process(my_task){
+	        	sprintf(str,"Hello World from Process [%d] with priority [%d]\r\n", my_task->pid, my_task->prio);
+			strcat(read_buf,str);
+		}
+		flag =1;
+		printk(KERN_INFO "%d",strlen(read_buf));
+		curr_ptr = read_buf;
+		kfree(str);
 	}
+	if((flag == 1) && (flag1==0)) 
+	{
+		printk(KERN_INFO "flag1 %d",flag1);
+		if((len+length) < strlen(read_buf))
+		{
+			
+			memcpy(buffer,curr_ptr,length);			
+			curr_ptr = curr_ptr + length+1;
+			len = len + length;
+			printk(KERN_INFO "%d",len);
+			return length;
+		}
+		else 
+		{
+			length = (read_buf + strlen(read_buf)) - curr_ptr;
+			memcpy(buffer,curr_ptr,length+1);
+			curr_ptr = curr_ptr + length+1;	
+			len = len + length;
+			flag1 = 1;
+			return length;
+			
+		}
+		
+	}
+	
+	kfree(read_buf);
 	return OK;
+	
 }
 
-static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t *off)
+static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
 {
 	printk(KERN_INFO "This operation not supported\r\n");
 	return OK;
@@ -66,6 +120,10 @@ static ssize_t device_write(struct file *filp, const char *buff, size_t len, lof
 
 static int device_release(struct inode *inode, struct file *file)
 {
+	printk(KERN_INFO "This device is released\r\n");
+	flag = 0;
+	flag1 = 0;
+	len = 0;
 	return OK;
 }
 
